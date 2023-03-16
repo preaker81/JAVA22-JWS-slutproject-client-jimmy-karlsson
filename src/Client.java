e7b22913e96bd114331263db47d6d2a5569615d3
Jimmy <preaker18@gmail.com>
2023-03-16 09:00:08 +0100

    Added comments

--- src/Client.java
+++ src/Client.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        // Declare and initialize socket, input/output streams, and buffered readers/writers
         Socket socket = null;
         InputStreamReader inputStreamReader = null;
         OutputStreamWriter outputStreamWriter = null;
@@ -14,31 +15,40 @@ public class Client {
         BufferedWriter bufferedWriter = null;
 
         try {
+            // Create a new socket with the server's address and port number
             socket = new Socket("localhost", 4321);
+            // Initialize input and output streams to read and write data from/to the server
             inputStreamReader = new InputStreamReader(socket.getInputStream());
             outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
 
+            // Create buffered readers/writers to read and write data from/to the server
             bufferedReader = new BufferedReader(inputStreamReader);
             bufferedWriter = new BufferedWriter(outputStreamWriter);
 
+            // Create a scanner object to read input from the user
             Scanner scanner = new Scanner(System.in);
 
+            // Loop until the user enters "quit"
             while (true) {
+                // Read input from the user and send it to the server
                 String message = scanner.nextLine();
                 bufferedWriter.write(message);
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
 
-                //This will wait for response from server
+                // Read the server's response and print it to the console
                 System.out.println(bufferedReader.readLine());
 
+                // If the user enters "quit", exit the loop
                 if (message.equalsIgnoreCase("quit")) {
                     break;
                 }
             }
         } catch (Exception e) {
+            // Print any exceptions that occur
             System.out.println(e);
         } finally {
+            // Close all open resources
             try {
                 if (socket != null) {
                     socket.close();
@@ -57,6 +67,7 @@ public class Client {
                 }
             }
             catch (Exception e){
+                // Print any exceptions that occur while closing resources
                 e.printStackTrace();
             }
         }
