a303697c229fcf73014b031718776a8cef63cca2
Jimmy <preaker18@gmail.com>
2023-03-28 14:10:27 +0200

    Commit 2023-03-28 Finished Comments in Test class

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -9,14 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Test class for the Client
 public class ClientTest {
-    // Initialize variables for output and input stream manipulation
+    // ByteArrayOutputStream and PrintStream for capturing and redirecting console output
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
+
+    // ByteArrayInputStream for simulating user input
     private final ByteArrayInputStream inContent;
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
 
     // Constructor for the test class
     public ClientTest() {
-        // Define the input to be used for the tests
+        // Prepare the simulated user input
         String input = "1\n4\n";
         inContent = new ByteArrayInputStream(input.getBytes());
     }
@@ -24,9 +29,20 @@ public class ClientTest {
     // Set up the streams before each test
     @BeforeEach
     public void setUpStreams() {
-        // Redirect the output and input streams
+        // Redirect the output and input streams to the custom streams
         System.setOut(new PrintStream(outContent));
         System.setIn(inContent);
+    }
+
+    // Set up the socket and its associated streams before each test
+    @BeforeEach
+    public void setUpSocketAndStreams() throws IOException {
+        // Create a socket and connect to the server
+        socket = new Socket("localhost", 10000);
+        // Initialize the BufferedWriter for writing to the socket
+        bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        // Initialize the BufferedReader for reading from the socket
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 
     // Restore the streams after each test
@@ -37,29 +53,39 @@ public class ClientTest {
         System.setIn(System.in);
     }
 
+    // Close the socket and its associated streams after each test
+    @AfterEach
+    public void closeSocketAndStreams() throws IOException {
+        // Close the BufferedReader
+        if (bufferedReader != null) {
+            bufferedReader.close();
+        }
+        // Close the BufferedWriter
+        if (bufferedWriter != null) {
+            bufferedWriter.close();
+        }
+        // Close the socket
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
     // Test the GET all books request
     @Test
     public void testGetAllBooks() throws IOException {
-        // Create a socket and connect to the server
-        Socket socket = new Socket("localhost", 10000);
-
-        // Initialize input and output streams
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-        // Create the GET request
+        // Prepare the GET request for all books
         String getRequest = "GET / HTTP/1.1\r\n";
         getRequest += "Connection: close\r\n";
         getRequest += "\r\n";
+
+        // Send the GET request
         bufferedWriter.write(getRequest);
         bufferedWriter.flush();
 
-        // Read the server response
         String line;
         String response = null;
 
+        // Read the server response and extract the status line
         while ((line = bufferedReader.readLine()) != null) {
             if (line.startsWith("HTTP/1.1")) {
                 response = line.substring("HTTP/1.1".length()).trim();
@@ -70,92 +96,80 @@ public class ClientTest {
             }
         }
 
-        // Close the socket
-        socket.close();
-
-        // Assert that the server response is as expected
+        // Assert that the server response is '200 OK'
         assertEquals("200 OK", response, "Server response should be '200 OK'");
     }
 
-    // Test the GET specific book request
+    // Test the GET request to retrieve a specific book by title
     @Test
     public void testGetSpecificBook() throws IOException {
-        // Create a socket and connect to the server
-        Socket socket = new Socket("localhost", 10000);
-
-        // Initialize input and output streams
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
         // Create the GET request for a specific book using the HTTP/1.1 protocol
         String getRequest = "GET /book?title=test HTTP/1.1\r\n";
         getRequest += "Connection: close\r\n";
         getRequest += "\r\n";
+
+        // Send the GET request to the server
         bufferedWriter.write(getRequest);
         bufferedWriter.flush();
 
-        // Read the server response line by line and store the status line in the 'response' variable
+        // Read the server response line by line
+        // Store the status line in the 'response' variable
         String line;
         String response = null;
 
         while ((line = bufferedReader.readLine()) != null) {
+            // Check if the line starts with "HTTP/1.1"
+            // If so, extract the status code and reason phrase
             if (line.startsWith("HTTP/1.1")) {
                 response = line.substring("HTTP/1.1".length()).trim();
             }
 
+            // If the line is empty, stop reading the response
             if (line.isEmpty()) {
                 break;
             }
         }
 
-        socket.close();
-
         // Check if the server response is '200 OK'
         assertEquals("200 OK", response, "Server response should be '200 OK'");
-
     }
 
+    // Test the POST request to add a new book
     @Test
     public void testPostRequest() throws IOException {
-        // Create a socket and connect to the server
-        Socket socket = new Socket("localhost", 10000);
-
-        // Initialize input and output streams for communication with the server
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
         // Define the JSON input for the POST request
         String jsonInput = "{\"title\":\"JUnit Test Book\",\"category\":\"Testing\",\"author\":\"John Doe\",\"words\":123}";
 
-        // Create the POST request using the HTTP/1.1 protocol, including headers for content type, content length, and connection
+        // Create the POST request using the HTTP/1.1 protocol
+        // Include headers for content type, content length, and connection
         String postRequest = "POST / HTTP/1.1\r\n";
         postRequest += "Content-Type: application/json\r\n";
         postRequest += "Content-Length: " + jsonInput.length() + "\r\n";
         postRequest += "Connection: close\r\n";
         postRequest += "\r\n";
         postRequest += jsonInput;
+
+        // Send the POST request to the server
         bufferedWriter.write(postRequest);
         bufferedWriter.flush();
 
-        // Read the server response line by line and store the status line in the 'response' variable
+        // Read the server response line by line
+        // Store the status line in the 'response' variable
         String line;
         String response = null;
 
         while ((line = bufferedReader.readLine()) != null) {
+            // Check if the line starts with "HTTP/1.1"
+            // If so, extract the status code and reason phrase
             if (line.startsWith("HTTP/1.1")) {
                 response = line.substring("HTTP/1.1".length()).trim();
             }
 
+            // If the line is empty, stop reading the response
             if (line.isEmpty()) {
                 break;
             }
         }
-
-        socket.close();
 
         // Check if the server response is '201 Created'
         assertEquals("201 Created", response, "Server response should be '201 Created'");
